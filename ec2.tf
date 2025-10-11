944df7f4e15b79869fabd65f169a3a863688c911
benjaminsteffens <ben.steffens@yahoo.com>
2025-10-11 19:39:17 -0400

    moding ec2 ami id

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "pipeec2" {
-  ami                         = "ami-0cfde0ea8edd312d4"
+  ami                         = "ami-052064a798f08f0d3"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
 
