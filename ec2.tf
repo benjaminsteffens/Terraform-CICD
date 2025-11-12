7f4533fda52834da8210c4272158bd08f1c0a190
benjaminsteffens <ben.steffens@yahoo.com>
2025-11-11 20:35:26 -0500

    updating ec2

--- ec2.tf
+++ ec2.tf
@@ -2,4 +2,13 @@ resource "aws_instance" "ec2_1" {
   ami           = "ami-0ecb62995f68bb549"
   instance_type = "t2.micro"
 
+  subnet_id = subnet-017f9e5639672982d 
+
+  vpc_security_group_ids = ["sg-0ffc5fa9ee33421ef"] 
+
+  tags = {
+    Name = "My-Fixed-EC2-Instance"
+  }
+}
+
 }
