476bc31ae879469ae5500a2da3276a8b3789e668
benjaminsteffens <ben.steffens@yahoo.com>
2025-11-12 11:49:41 -0500

    fixing ec2

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,7 @@
 resource "aws_instance" "Terraform-CICD" {
   ami           = "ami-0ecb62995f68bb549" 
-  name          = "Terraform-CICD"    
   instance_type = "t2.micro" 
+
+  tags = {
+    Name = "webserver1"
 }
