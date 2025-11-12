2d8cf863c64f1f24ac6834889c8b5c3ba844bf12
benjaminsteffens <ben.steffens@yahoo.com>
2025-11-11 21:10:30 -0500

    fixing ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,7 @@
-resource "aws_instance" "ec2_1" {
-  ami                         = "ami-00ecb62995f68bb549"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  vpc_security_group_ids      = ["sg-0ffc5fa9ee33421ef"] 
-  subnet_id                   = aws_subnet.sub_1.id  
+resource "aws_instance" "Terraform-CICD" {
+  # 1. Required: Specify the OS image (AMI ID)
+  ami           = "ami-00ecb62995f68bb549" 
   
-  tags = {
-    Name = "Terraform-CICD"
-  }
+  # 2. Required: Specify the instance size (type)
+  instance_type = "t2.micro" 
 }
