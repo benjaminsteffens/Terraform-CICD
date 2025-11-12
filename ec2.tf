5019782634857afa7445cf4d390387179c43ea46
benjaminsteffens <ben.steffens@yahoo.com>
2025-11-11 21:03:14 -0500

    fixing ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,8 +3,7 @@ resource "aws_instance" "ec2_1" {
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = ["sg-0ffc5fa9ee33421ef"]
-  subnet_id                   = subnet_id = aws_subnet.sub_1.id
-
+subnet_id                   = subnet_id = aws_subnet.sub_1.id
   tags = {
     Name = "Terraform-CICD"
   }
