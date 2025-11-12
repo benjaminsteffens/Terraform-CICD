8e566044152bd54cf66997e53049171ac57cf4d0
benjaminsteffens <ben.steffens@yahoo.com>
2025-11-12 11:43:33 -0500

    fixing ec2

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,5 @@
 resource "aws_instance" "Terraform-CICD" {
-  # 1. Required: Specify the OS image (AMI ID)
   ami           = "ami-0ecb62995f68bb549" 
-  
-  # 2. Required: Specify the instance size (type)
+  name          = "Terraform-CICD"    
   instance_type = "t2.micro" 
 }
